58382a66e5d9877b179e7cecf706a389fef56501
Filipe Lima <cimofelipe4@gmail.com>
2022-03-28 22:01:55 -0300

    Adicionado dist

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,6 @@ const Home = () => {
   const API_KEY = "api_key=06ae2dc755080f5934364ba0fb9232fb";
   const BASE_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&${API_KEY}&language=pt-BR`;
   const URL_IMG = `https://image.tmdb.org/t/p/w500`;
-  const RedirectURL = `https://api.themoviedb.org/3/movie/?api_key=06ae2dc755080f5934364ba0fb9232fb`
-  const [d, setD] = useState(null)
 
   useEffect(() => {
     async function getData() {
@@ -29,7 +27,6 @@ const Home = () => {
             data.results.map(
               ({ title, vote_average, poster_path, overview, id }) => (
                 <div key={title} className="item-movies">
-                  
                   <div className="movie">
                     <img
                       src={URL_IMG + poster_path}
@@ -40,7 +37,15 @@ const Home = () => {
                   </div>
 
                   <div className="area-movie-bottom">
-                    <p className="text-movie-bottom ">Avaliação: <span className="span-vote" style={{color: vote_average >= 6 ? 'green' : 'red'}}>{vote_average}</span></p>
+                    <p className="text-movie-bottom ">
+                      Avaliação:{" "}
+                      <span
+                        className="span-vote"
+                        style={{ color: vote_average >= 6 ? "green" : "red" }}
+                      >
+                        {vote_average}
+                      </span>
+                    </p>
                     <h1 className="text-movie-bottom">{title}</h1>
                   </div>
                 </div>
